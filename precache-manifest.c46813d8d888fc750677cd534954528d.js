2812b486882f000940b1a711ad64332ee262d41a
TheNameIsGav <spqr.dominus@gmail.com>
2022-05-11 15:21:53 -0500

    Updates

---
+++ precache-manifest.c46813d8d888fc750677cd534954528d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e0f114bdaad1d7daed2",
+    "url": "/Spotify_Uncovered_v2/static/css/main.57fbc3f4.chunk.css"
+  },
+  {
+    "revision": "6e0f114bdaad1d7daed2",
+    "url": "/Spotify_Uncovered_v2/static/js/main.6e0f114b.chunk.js"
+  },
+  {
+    "revision": "2229431793fa7dafa21a",
+    "url": "/Spotify_Uncovered_v2/static/css/1.1353dcee.chunk.css"
+  },
+  {
+    "revision": "2229431793fa7dafa21a",
+    "url": "/Spotify_Uncovered_v2/static/js/1.22294317.chunk.js"
+  },
+  {
+    "revision": "f3307719dd85bc1da9b2",
+    "url": "/Spotify_Uncovered_v2/static/js/runtime~main.f3307719.js"
+  },
+  {
+    "revision": "634fb339404cbf711808f95e45493814",
+    "url": "/Spotify_Uncovered_v2/index.html"
+  }
+];
